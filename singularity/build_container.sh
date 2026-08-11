3af9abfd921975756c2da8ad351bc689d3b9035e
eskehaack <eskehaack@gmail.com>
2026-08-11 17:26:40 +0300

    build from singularity container

---
+++ singularity/build_container.sh
@@ -0,0 +1,23 @@
+srun \
+    --account=project_465002687 \
+    --partition=small \
+    --nodes=1 \
+    --ntasks=1 \
+    --cpus-per-task=8 \
+    --mem=256G \
+    --time=02:00:00 \
+    bash -lc '
+    module purge
+    module load CrayEnv
+    module load PRoot
+
+    mkdir -p "/tmp/$USER/singularity-build"
+    export SINGULARITY_TMPDIR="/tmp/$USER/singularity-build"
+    export SINGULARITY_CACHEDIR="/tmp/$USER/singularity-build"
+
+    mkdir -p "/tmp/$USER/mpl"
+	export MPLCONFIGDIR="/tmp/$USER/mpl"
+
+	cd ~/Desktop/sirius
+	singularity build singularity/docker.sif singularity/docker.def
+    '
